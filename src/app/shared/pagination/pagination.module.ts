476f5cecd8ef1bb5d9d31001cd8562cb6b470724
xuhuan <xuhuan@vanda-tec.com>
2018-01-22 17:25:38 +0800

    分页组件

---
+++ src/app/shared/pagination/pagination.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { PaginationComponent } from './pagination.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [PaginationComponent],
+  exports: [
+    PaginationComponent
+  ],
+})
+export class PaginationModule { }
