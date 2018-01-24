4397790ceae81965883ebea137f64a2535fad4c1
caojie <smart_xiao_jie@foxmail.com>
2018-01-24 16:35:22 +0800

    Merge remote-tracking branch 'remotes/origin/test' into cj_branch
    # Conflicts:
    #	src/app/core/map/map.component.ts
    #	src/app/pages/intermediate/intermediate.module.ts
    #	src/app/pages/intermediate/intermediate.service.ts

--- src/app/pages/intermediate/industry-board/industry-board.component.ts
+++ src/app/pages/intermediate/industry-board/industry-board.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./industry-board.component.css']
 })
 export class IndustryBoardComponent implements OnInit {
-
+  isShowIndustryMenu: boolean = true;
+  isShowParkMenu: boolean = false;
+  isShowLandMenu: boolean = false;
   constructor() { }
 
   ngOnInit() {
+  }
+  showIndustryMenus(flag) {
+    switch (flag) {
+      case 'IndustryMenu':
+        this.isShowParkMenu = false;
+        this.isShowLandMenu = false;
+        this.isShowIndustryMenu = true;
+        break;
+      case 'ParkMenu':
+        this.isShowLandMenu = false;
+        this.isShowIndustryMenu = false;
+        this.isShowParkMenu = true;
+        break;
+      case 'LandMenu':
+        this.isShowIndustryMenu = false;
+        this.isShowParkMenu = false;
+        this.isShowLandMenu = true;
+        break;
+    }
   }
 
 }
