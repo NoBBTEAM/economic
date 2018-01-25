211c3d463b036633b4641cfadc3c165231f689d8
xuhuan <xuhuan@vanda-tec.com>
2018-01-25 14:33:57 +0800

    最左边悬浮三个按钮，企业培育追踪放到微观里面。

---
+++ src/app/pages/microcosmic/event-notice/breed-track/breed-track.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breed-track',
+  templateUrl: './breed-track.component.html',
+  styleUrls: ['./breed-track.component.css']
+})
+export class BreedTrackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
